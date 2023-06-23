ce6a93547effef3c228b076483b777b676149a3d
Juraj Siroky <siroky.juraj0@gmail.com>
2023-06-23 22:16:55 +0200

    Finished hw04

--- homework/hw04/hw04.py
+++ homework/hw04/hw04.py
@@ -170,6 +170,11 @@ def totals_tree(m):
     True
     """
     "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return tree(mass(m))
+    else:
+        branches = [totals_tree(end(f(m))) for f in [left, right]]
+        return tree(sum([label(b) for b in branches]), branches)
 
 
 def replace_loki_at_leaf(t, lokis_replacement):
@@ -202,6 +207,10 @@ def replace_loki_at_leaf(t, lokis_replacement):
     True
     """
     "*** YOUR CODE HERE ***"
+    print(f"DEBUG: {branches(t)}")
+    if is_leaf(t) and label(t) == "loki":
+        return tree(lokis_replacement)
+    return tree(label(t), [replace_loki_at_leaf(b, lokis_replacement) for b in branches(t)])
 
 
 
@@ -237,7 +246,14 @@ def has_path(t, word):
     """
     assert len(word) > 0, 'no path for empty word.'
     "*** YOUR CODE HERE ***"
-
+    if label(t) != word[0]:
+        return False
+    elif len(word) == 1:
+        return True
+    for b in branches(t):
+        if has_path(b, word[1:]):
+            return True
+    return False
 
 def str_interval(x):
     """Return a string representation of interval x."""
@@ -261,11 +277,13 @@ def interval(a, b):
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[0]
 
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[1]
 
 
 def str_interval(x):
@@ -284,17 +302,20 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y) # 2
+    p2 = lower_bound(x) * upper_bound(y) # 4
+    p3 = upper_bound(x) * lower_bound(y) # -4
+    p4 = upper_bound(x) * upper_bound(y) # -8
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
+    negative_y = interval(-upper_bound(y), -lower_bound(y))
+    return add_interval(x, negative_y)
+
 
 
 def div_interval(x, y):
@@ -302,7 +323,8 @@ def div_interval(x, y):
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
-    reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
+    assert not (lower_bound(y) <= 0 <= upper_bound(y)), 'Divide by zero'
+    reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
 
@@ -326,8 +348,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 2)
+    r2 = interval(3, 4)
     return r1, r2
 
 
