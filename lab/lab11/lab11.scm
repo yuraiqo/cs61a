f01187e9ebf12579a1ccfb730501a195dfdb60fe
Juraj Siroky <siroky.juraj0@gmail.com>
2023-07-17 00:42:09 +0200

    Finished lab11

---
+++ lab/lab11/lab11.scm
@@ -0,0 +1,32 @@
+(define (over-or-under num1 num2)
+  (cond
+    ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    ((> num1 num2) 1))
+)
+
+(define (make-adder num)
+  (lambda (inc) (+ num inc))
+)
+
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
+
+(define (repeat f n)
+  (if (< n 1)
+    (lambda (x) x)
+    (composed f (repeat f (- n 1))))
+)
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) 'YOUR-CODE-HERE)
