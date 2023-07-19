daf06561fd1c9203ef831a100360b7b7474527ec
Juraj Siroky <siroky.juraj0@gmail.com>
2023-07-19 23:45:46 +0200

    Finished hw09

--- homework/hw09/hw09.scm
+++ homework/hw09/hw09.scm
@@ -6,8 +6,25 @@
     )
 )
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (define lst ())
+    (cond
+        ((null? s) lst)
+        ((pred (car s)) (append lst (list (car s)) (my-filter pred (cdr s))))
+        (else (append lst (my-filter pred (cdr s))))
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond 
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+    )
+)
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst)
+    (if (null? lst) lst
+        (cons (car lst)
+            (no-repeats (my-filter (lambda (x) (not (= (car lst) x))) (cdr lst)))))
+)
