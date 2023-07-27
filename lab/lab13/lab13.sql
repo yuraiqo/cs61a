b27ce58cfce3125932c4f553f50be33a52573735
Juraj Siroky <siroky.juraj0@gmail.com>
2023-07-27 23:56:56 +0200

    Finished lab13 and hw10

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -9,15 +9,16 @@ CREATE TABLE bluedog_songs AS
 
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest FROM students GROUP BY smallest HAVING COUNT(*) = 1;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color FROM students AS a, students AS b
+    WHERE a.time < b.time AND a.pet = b.pet AND a.song = b.song;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT s.seven FROM students AS s, numbers AS c WHERE s.number = 7 AND c.'7' = 'True' AND s.time = c.time;
 
 
 CREATE TABLE average_prices AS
